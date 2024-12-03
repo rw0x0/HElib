c215e20455786637205d5cc4851f787ebac1f845
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-12-03 15:01:27 +0100

    .

---
+++ include/helib/c/ctxt.h
@@ -0,0 +1,3 @@
+#include "helib/c/c.h"
+
+C_FUNC ctxt_destroy(void *ctxt);
--- src/c/pubkey.cpp
+++ src/c/pubkey.cpp
@@ -25,3 +25,20 @@ C_FUNC pubkey_from_seckey(void **pubkey, void *seckey) {
     *pubkey = new helib::PubKey(*seckey_);
     return S_OK;
 }
+
+C_FUNC pubkey_encrypt(void **ctxt, void *pubkey, void *ptxt_ZZ) {
+    helib::PubKey *pubkey_ = FromVoid<helib::PubKey>(pubkey);
+    IfNullRet(pubkey_, E_POINTER);
+    IfNullRet(ctxt, E_POINTER);
+
+    NTL::ZZ *ptxt_ZZ_ = FromVoid<NTL::ZZ>(ptxt_ZZ);
+    IfNullRet(ptxt_ZZ_, E_POINTER);
+
+    *ctxt = new helib::Ctxt(*pubkey_);
+    helib::Ctxt *ctxt_ = FromVoid<helib::Ctxt>(*ctxt);
+    IfNullRet(ctxt_, E_POINTER);
+
+    NTL::ZZX ptxt = NTL::ZZX(*ptxt_ZZ_);
+    pubkey_->Encrypt(*ctxt_, ptxt);
+    return S_OK;
+}
