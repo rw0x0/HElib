e2fdaa884581e8f7a537f5590100b2d3570b6194
Roman Walch <9820846+rw0x0@users.noreply.github.com>
2024-12-04 11:32:39 +0100

    chore: let decrypt allocate the memory of the result

--- include/helib/c_seckey.h
+++ include/helib/c_seckey.h
@@ -8,4 +8,4 @@ C_FUNC seckey_destroy(void *seckey);
 
 C_FUNC seckey_encrypt(void **ctxt, void *seckey, void *ptxt_ZZ);
 
-C_FUNC seckey_decrypt(void *ptxt_ZZ, void *seckey, void *ctxt);
+C_FUNC seckey_decrypt(void **ptxt_ZZ, void *seckey, void *ctxt);
--- src/c_seckey.cpp
+++ src/c_seckey.cpp
@@ -37,17 +37,19 @@ C_FUNC seckey_encrypt(void **ctxt, void *seckey, void *ptxt_ZZ) {
     return S_OK;
 }
 
-C_FUNC seckey_decrypt(void *ptxt_ZZ, void *seckey, void *ctxt) {
+C_FUNC seckey_decrypt(void **ptxt_ZZ, void *seckey, void *ctxt) {
     helib::SecKey *seckey_ = FromVoid<helib::SecKey>(seckey);
     IfNullRet(seckey_, E_POINTER);
     helib::Ctxt *ctxt_ = FromVoid<helib::Ctxt>(ctxt);
     IfNullRet(ctxt_, E_POINTER);
 
-    NTL::ZZ *ptxt_ZZ_ = FromVoid<NTL::ZZ>(ptxt_ZZ);
-    IfNullRet(ptxt_ZZ_, E_POINTER);
+    IfNullRet(ptxt_ZZ, E_POINTER);
+    *ptxt_ZZ = new NTL::ZZ;
+    NTL::ZZ *ZZ = FromVoid<NTL::ZZ>(*ptxt_ZZ);
+    IfNullRet(ZZ, E_POINTER);
 
     NTL::ZZX decrypted;
     seckey_->Decrypt(decrypted, *ctxt_);
-    *ptxt_ZZ_ = decrypted[0];
+    *ZZ = decrypted[0];
     return S_OK;
 }
